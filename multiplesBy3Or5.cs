2d5861fe183e06413cf213a6650b110c4e28b6bc
JelenaBarinova <fiodorova.jelena@gmail.com>
2015-01-21 15:33:14 -0800

    Refactored to support big amount of big numbers

--- multiplesBy3Or5.cs
+++ multiplesBy3Or5.cs
@@ -5,13 +5,35 @@ class Solution {
     static void Main(String[] args) 
     {
         var t_count = Int32.Parse(Console.ReadLine());
+        
+        Dictionary<int, Int64> sumByNumbers = new Dictionary<int, Int64>();
+        sumByNumbers.Add(0, 0);
+        int maxCachedNumber = 0;
+
         for (int t = 0; t < t_count; t++)
         {
-            var n = Int64.Parse(Console.ReadLine());   
-            long sum = 0;
-            for (int i = 3; i < n; i += 3) sum += i;
-            for (int i = 5; i < n; i += 5) if (i % 3 != 0) sum += i;
-            
+            int n = Int32.Parse(Console.ReadLine());   
+            Int64 sum = 0;
+            n--;
+            if (n <= maxCachedNumber)
+            {
+                int i = n;
+                while (i > 0 && (i % 3 != 0 && i % 5 != 0)) i--;
+                sum = sumByNumbers[i];
+            }
+            else
+            {
+                sum = sumByNumbers[maxCachedNumber];
+                for (int i = maxCachedNumber + 1; i <= n; i++)
+                {
+                    if (i % 3 == 0 || i % 5 == 0)
+                    {
+                        sum += i;
+                        sumByNumbers.Add(i, sum);  
+                        maxCachedNumber = Math.Max(maxCachedNumber, i);
+                    }
+                }          
+            }
             Console.WriteLine(sum);
         }
     }
